2d9d0c03513b21dbe2f6a56fe7a71dd9444e1968
sudo.park <sudo.park@kakao.com>
2022-04-09 12:35:07 +0900

    todo edit 추가

--- src/Todo.js
+++ src/Todo.js
@@ -8,12 +8,38 @@ class Todo extends React.Component {
     constructor(props) {
         super(props);
         // state는 리엑트가 관리하는 오브젝트 => 추후에 변경될 수 있는값 => 변경이후 다시 랜더링
-        this.state = { item: props.item };
+        this.state = { item: props.item, readOnly: true };
         this.delete = props.delete;
     }
 
     deleteEventHandler = () => {
         this.delete(this.state.item)
+    }
+
+    toggleChecked = (e) => {
+        const thisItem = this.state.item;
+        thisItem.done = !thisItem.done;
+        this.setState({item: thisItem});
+    }
+
+    offReadOnlyMode = () => {
+        console.log("Event!", this.state.readOnly);
+        this.setState({readOnly: false}, () => {
+            console.log("ReadOnly? ", this.state.readOnly);
+        });
+    }
+
+    editEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({item: thisItem});
+    }
+
+    enterKeyEventHandler = (e) => {
+        if (e.key == "Enter") {
+            console.log("turn on readOnly");
+            this.setState({readOnly: true});
+        }
     }
 
     render() {
@@ -22,10 +48,19 @@ class Todo extends React.Component {
 
         return (
             <ListItem>
-                <Checkbox checked={item.done}/>
+                <Checkbox 
+                    checked={item.done} disableFocusRipple
+                    onChange={this.toggleChecked}
+                />
                 <ListItemText>
                     <InputBase
-                        inputProps={{"aria-label": "naked"}}
+                        inputProps={{
+                            "aria-label": "naked",
+                            readOnly: this.state.readOnly
+                        }}
+                        onClick={this.offReadOnlyMode}
+                        onChange={this.editEventHandler}
+                        onKeyPress={this.enterKeyEventHandler}
                         type="text"
                         id={item.id}
                         name={item.id}
