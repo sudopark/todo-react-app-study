840e27d078308b5471ebae68a5e6db382f971fed
sudo.park <sudo.park@kakao.com>
2022-04-15 20:35:36 +0900

    로그인 페이지 추가 및 AppRouter 구성(React-Router-dom 설치)

---
+++ src/AppRouter.js
@@ -0,0 +1,44 @@
+
+import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Login from "./Login";
+import Box from "@material-ui/core/Box";
+import { Typography } from "@material-ui/core";
+
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright "}
+            sudopark, {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    )
+}
+
+
+class AppRouter extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Router>
+                    <div>
+                        <Routes>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/" element={<App/>}/>
+                        </Routes>
+                    </div>
+                    <Box mt={5}>
+                        <Copyright />
+                    </Box>
+                </Router>
+            </div>
+        );
+    }
+}
+
+
+export default AppRouter;
---
+++ src/Login.js
@@ -0,0 +1,17 @@
+
+
+import React from "react";
+
+class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return <p>로그인 페이지</p>;
+    }
+}
+
+export default Login;
